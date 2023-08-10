0d600911f105f8c0904399cab06a94227133ae61
Kristian Kermedchiev <106478447+KristianKermedchiev@users.noreply.github.com>
2023-08-10 15:47:46 +0300

    Navbar created.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import Navbar from './Components/Navbar';
-import Section from './Components/Section';
-
-const AppContainer = styled.div`
-  font-family: Arial, sans-serif;
-`;
+import './App.css';
 
 function App() {
   return (
-    <AppContainer>
-      <Navbar />
-      <Section
-        title="Welcome to My Website"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit."
-      />
-      <Section
-        title="About Us"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit."
-      />
-      <Section
-        title="Our Services"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit."
-      />
-      <Section
-        title="Contact Us"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit."
-      />
-    </AppContainer>
-  );
-}
+   <Navbar/>
+)}
 
 export default App;
--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,116 +1,31 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
+import '../Styles/Navbar.css';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <nav className="navbar">
+      <div className="logo">Logo</div>
+      <div className={`menu ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+      <div className={`burger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
   );
-}
-export default ResponsiveAppBar;
+};
+
+export default Navbar;
